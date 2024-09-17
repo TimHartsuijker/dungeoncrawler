7d83eb2b29bfc91ff3890b5f36b4b0f1de935b4d
Tim Hartsuijker <tim1hartsuijker@gmail.com>
2024-09-17 14:54:36 +0200

    added class for rooms

--- main.py
+++ main.py
@@ -59,3 +59,16 @@ class Weapons:
         Self.Name = name
         Self.Description = description
         Self.Damage = damage
+
+# rooms (x, y, monsters, items)
+class rooms:
+    X = None
+    Y = None
+    Monsters = None
+    Items = None
+
+    def __init__(Self, x, y, monsters, items):
+        Self.X = x
+        Self.y = y
+        Self.Monsters = monsters
+        Self.Items = items
