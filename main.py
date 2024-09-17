f13febf803f64627351a69b342725d4fb0e8d993
BramDekkersJojo <brammetjed@gmail.com>
2024-09-17 14:12:36 +0200

    Started item class

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ Monster1 = Monster("Demon dog", 3, 5)
 Monster1Name = Monster1.Name
 
 print(Monster1Name)
+
+class Item:
+    Name = None
+    Rarity = None
+    Description = None
+    Effect = None
+    Modifier = None
+
+    def __init__(self, name, rarity, description, effect, modifier):
+        self.Name = name
+        self.Rarity = rarity
+        self.Description = description
+        self.Effect = effect
+        self.Modifier = modifier
--- todo.py
+++ todo.py
@@ -1,6 +1,6 @@
 # players (x, y, hp, weapon, item, armour, turn)
 # items (rarity, name, description, effect, modifier)
 # armour (name, modifier)
-# monsters (name, hp, damage)
+# monsters (name, hp, damage) |  Class made
 # rooms (x, y, monsters, items)
 # weapons (rarity, name, description, damage)
