ce911cd82685a2c7fcc0ee353afe32d362a1b810
Tim Hartsuijker <tim1hartsuijker@gmail.com>
2024-09-17 15:00:27 +0200

    todo updated

--- todo.py
+++ todo.py
@@ -2,5 +2,5 @@
 # items (rarity, name, description, effect, modifier) | Class made
 # armour (name, modifier) | Class made
 # monsters (name, hp, damage) |  Class made
-# rooms (x, y, monsters, items)
+# rooms (x, y, monsters, items) | Class made
 # weapons (rarity, name, description, damage) | Class made
